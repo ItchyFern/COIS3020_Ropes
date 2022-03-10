bbb168eb6fc748cb5f60197ca71794228b7a1e17
seth <seth@sethpc.ipv6.telus.net>
2022-03-10 11:58:40 -0500

    create new dotnet console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COIS3020_Ropes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
